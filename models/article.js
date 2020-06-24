85a5dd80a973a18092de79c10a6dcc89822f7169
Tony <elkobrat@protonmail.com>
2020-06-24 11:59:13 +1000

    added mongo model for articles

---
+++ models/article.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const validate = require('mongoose-validator');
+
+const urlValidator = [
+  validate({
+    validator: 'isURL',
+    message: 'Невалидный URL изображения',
+  }),
+];
+
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  title: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: urlValidator,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: urlValidator,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+    select: true,
+  },
+});
+
+module.exports = mongoose.model('article', articleSchema);
