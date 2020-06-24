52708541d8d00a5acbdaf0e70d150b9713226b9d
Tony <elkobrat@protonmail.com>
2020-06-24 12:26:02 +1000

    added basic routers

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
-// Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+const { PORT = 3000 } = process.env;
 const app = express();
+app.use(limiter);
+app.use(helmet());
+app.use(cookieParser());
+app.use(limiter);
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
