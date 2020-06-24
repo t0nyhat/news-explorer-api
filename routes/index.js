d3aff555edd7782f47e97b12b9246e947624b381
Tony <elkobrat@protonmail.com>
2020-06-24 18:07:15 +1000

    added signup signin routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const users = require('./users');
 const articles = require('./articles');
+const { createUser, login } = require('../controllers/users');
+
+router.post('/signin', login);
+router.post('/signup', createUser);
 
 router.use('/articles', articles);
 router.use('/users', users);
