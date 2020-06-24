673df0e0eac3c0535e6d1616a790c4858c2d54c0
Tony <elkobrat@protonmail.com>
2020-06-24 19:38:16 +1000

    added validation middleware

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { createArticle, getAllArticles, deleteArticleById } = require('../controllers/articles');
+const { createArticleValidation, deleteArticleValidation } = require('../middlewares/validation');
 
-router.post('/', createArticle);
+router.post('/', createArticleValidation, createArticle);
 router.get('/', getAllArticles);
-router.delete('/:articleId', deleteArticleById);
+router.delete('/:articleId', deleteArticleValidation, deleteArticleById);
 
 module.exports = router;
