1515b49fdc5178b30c72348eac605933d51d69fc
Tony <elkobrat@protonmail.com>
2020-06-24 18:09:55 +1000

    some debug code

--- controllers/articles.js
+++ controllers/articles.js
@@ -6,16 +6,16 @@ const createArticle = (req, res, next) => {
   const {
     keyword, title, text, date, source, link, image,
   } = req.body;
-  const owner = '5ef2cab5d1dacf6ac43e5764';
+  const owner = '5ef30481b10d1776e9fb2558';
   Article.create({
     keyword, title, text, date, source, link, image, owner,
   })
-    .then((article) => res.send({ data: article }))
+    .then((article) => res.send({ article: article.omitPrivate() }))
     .catch(next);
 };
 
 const getAllArticles = (req, res, next) => {
-  const owner = '5ef2cab5d1dacf6ac43e5764';
+  const owner = '5ef30481b10d1776e9fb2558';
   Article.find({ owner })
     .then((articles) => res.send({ articles }))
     .catch(next);
