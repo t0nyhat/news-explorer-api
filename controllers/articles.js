ce1432978b1d976f2002becf9cd488a4b07db1e8
Tony <elkobrat@protonmail.com>
2020-06-24 12:37:54 +1000

    added createArticle method

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,3 +1,16 @@
 const Article = require('../models/article');
 
+module.exports.createArticle = (req, res, next) => {
+  const {
+    keyword, title, text, date, source, link, image,
+  } = req.body;
+  const owner = req.user._id;
+
+  Article.create({
+    keyword, title, text, date, source, link, image, owner,
+  })
+    .then((article) => res.send({ data: article }))
+    .catch(next);
+};
+
 module.exports = {};
