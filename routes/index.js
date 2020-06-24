daaee8c50a008ed0a8ea61813cedd835142dda06
Tony <elkobrat@protonmail.com>
2020-06-24 19:20:03 +1000

    added auth using

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 const users = require('./users');
 const articles = require('./articles');
 const { createUser, login } = require('../controllers/users');
+const auth = require('../middlewares/auth');
 
 router.post('/signin', login);
 router.post('/signup', createUser);
+
+router.use(auth);
 
 router.use('/articles', articles);
 router.use('/users', users);
