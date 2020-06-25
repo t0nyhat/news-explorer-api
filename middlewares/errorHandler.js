86964b0ab7e58567e4781165f55ebf7d222bc8f3
Tony <elkobrat@protonmail.com>
2020-06-25 15:29:38 +1000

    updated errors messages using constants

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
+const { NOT_UNIQUE_EMAIL, SERVER_ERROR } = require('../constants/constants');
 
 const errorHandler = (err, req, res, next) => {
-  const { code } = err;
-  let { statusCode = 500, message } = err;
+  let { statusCode = 500 } = err;
+  const { message } = err;
 
   if (err instanceof mongoose.Error.ValidationError || err instanceof mongoose.Error.CastError) {
     statusCode = 400;
   }
-  if (code === 11000) {
+  if (message.includes(NOT_UNIQUE_EMAIL)) {
     statusCode = 409;
-    message = 'Данный email уже используется';
   }
 
   res
     .status(statusCode)
     .send({
       message: statusCode === 500
-        ? 'На сервере произошла ошибка'
+        ? SERVER_ERROR
         : message,
     });
   next();
