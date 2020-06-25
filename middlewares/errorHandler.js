764db0a70fa7a2ae9387bc57151836c4833199ca
Tony <elkobrat@protonmail.com>
2020-06-25 16:39:21 +1000

    updated  error messages

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
-const { NOT_UNIQUE_EMAIL, SERVER_ERROR } = require('../constants/constants');
+const { NOT_UNIQUE_EMAIL, SERVER_ERROR, VALIDATION_ERROR } = require('../constants/constants');
 
 const errorHandler = (err, req, res, next) => {
-  let { statusCode = 500 } = err;
-  const { message } = err;
+  let { statusCode = 500, message } = err;
 
   if (err instanceof mongoose.Error.ValidationError || err instanceof mongoose.Error.CastError) {
+    message = `${VALIDATION_ERROR}  ${message}`;
     statusCode = 400;
   }
-  if (message.includes(NOT_UNIQUE_EMAIL)) {
+  if (message.includes('to be unique')) {
+    message = NOT_UNIQUE_EMAIL;
     statusCode = 409;
   }
 
