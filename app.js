8218ec672d6613132e25e98f41716be8aa10eac0
Tony <elkobrat@protonmail.com>
2020-06-25 15:27:52 +1000

    removed limiter settings, updated database url

--- app.js
+++ app.js
@@ -3,28 +3,39 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
+const { errors } = require('celebrate');
+const limiter = require('./middlewares/limiter');
 const router = require('./routes/index');
+const { errorHandler } = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { PORT, DATABASE_URL } = require('./config/config');
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-});
-const { PORT = 3000 } = process.env;
 const app = express();
+
 app.use(limiter);
 app.use(helmet());
+
 app.use(cookieParser());
-app.use(limiter);
-mongoose.connect('mongodb://localhost:27017/newsdb', {
+
+mongoose.connect(DATABASE_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogger);
+
 app.use(router);
+
+app.use(errorLogger);
+
+app.use(errors());
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
