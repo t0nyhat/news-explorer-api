e6b39842303f1a4c12a9b9c7dc1dc57fb911e263
Tony <elkobrat@protonmail.com>
2020-06-25 15:30:54 +1000

    updated errors messages using constants

--- models/user.js
+++ models/user.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const validate = require('mongoose-validator');
 const bcrypt = require('bcryptjs');
+const uniqueValidator = require('mongoose-unique-validator');
 const UnauthorisedError = require('../errors/unauthorised-err');
+const { INVALID_EMAIL, USER_NOT_FOUND, WRONG_MAIL_OR_PASS } = require('../constants/constants');
 
 const emailValidator = [
   validate({
     validator: 'isEmail',
-    message: 'Невалидный Email',
+    message: INVALID_EMAIL,
   }),
 ];
 const userSchema = new mongoose.Schema({
@@ -33,13 +35,13 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(email,
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new UnauthorisedError('Пользователь не найден'));
+        return Promise.reject(new UnauthorisedError(USER_NOT_FOUND));
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new UnauthorisedError('Неправильные почта или пароль'));
+            return Promise.reject(new UnauthorisedError(WRONG_MAIL_OR_PASS));
           }
 
           return user;
@@ -52,4 +54,6 @@ userSchema.methods.omitPrivate = function omitPrivate() {
   delete obj.password;
   return obj;
 };
+
+userSchema.plugin(uniqueValidator);
 module.exports = mongoose.model('user', userSchema);
