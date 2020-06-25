31d43306ae19ae883d1bc6cc499e9261b7565787
Tony <elkobrat@protonmail.com>
2020-06-25 15:28:46 +1000

    added file with constants

---
+++ constants/constants.js
@@ -0,0 +1,21 @@
+const EMPTY = 'Ошибка! Поле должно быть заполнено.';
+const INVALID_LINK = 'Неверный формат ссылки';
+const INVALID_EMAIL = 'Неправильный формат почты';
+const SOURCE_NOT_FOUND = 'Запрашиваемый ресурс не найден';
+const PERMISSION_DENIED = 'Невозможно удалить ресурс. Отсутсвуют права!';
+const USER_NOT_FOUND = 'Пользователь не найден';
+const WRONG_MAIL_OR_PASS = 'Неправильная почта или пароль';
+const NOT_UNIQUE_EMAIL = 'Пользователь с такой почтой уже существует';
+const SERVER_ERROR = 'На сервере произошла ошибка';
+
+module.exports = {
+  INVALID_LINK,
+  USER_NOT_FOUND,
+  PERMISSION_DENIED,
+  INVALID_EMAIL,
+  NOT_UNIQUE_EMAIL,
+  SERVER_ERROR,
+  WRONG_MAIL_OR_PASS,
+  SOURCE_NOT_FOUND,
+  EMPTY,
+};
